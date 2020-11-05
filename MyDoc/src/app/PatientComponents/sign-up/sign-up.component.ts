9797262183a2fa865f04b71d75853701f4a55ec7
HamdiBenkahla <68249436+HamdiBenkahla@users.noreply.github.com>
2020-11-05 23:45:14 +0100

    adding NgModel & NgForm on sINup patient

--- MyDoc/src/app/PatientComponents/sign-up/sign-up.component.ts
+++ MyDoc/src/app/PatientComponents/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms'
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value)
+    const name = form.value['name'];
+    const password = form.value['password'];
+    const email = form.value['email'];
+    const adress = form.value['adress'];
+    const city = form.value['city'];
+    const phone = form.value['tele']
+    const accountBanc = form.value['accountBanc'];
+}
 
 }
