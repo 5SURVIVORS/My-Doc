e6e866a3da20add3bda297a300309295835d50be
HamdiBenkahla <68249436+HamdiBenkahla@users.noreply.github.com>
2020-11-06 21:00:08 +0100

    link between front & back  done

--- MyDoc/src/app/services/doc-auth.service.ts
+++ MyDoc/src/app/services/doc-auth.service.ts
@@ -2,9 +2,7 @@ import {HttpClient} from '@angular/common/http'
 import { Injectable } from '@angular/core';
 import {Subject} from  'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 
 export class AuthDocService {
 
@@ -13,11 +11,11 @@ export class AuthDocService {
   constructor(private http : HttpClient) { }
 
 public docSignIn (info){
-  return this.http.post('/doctors/login',info)
+  return this.http.post('http://localhost:3000/doctors/login',info)
 
 }
 public docSingUp(options){
-  return this.http.post('/doctors/register', options)
+  return this.http.post('http://localhost:3000/doctors/register', options)
 }
 
 }
